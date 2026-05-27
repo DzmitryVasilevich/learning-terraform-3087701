588b556da79fc7591a962895445112c4893f5356
DzmitryVasilevich <51486060+DzmitryVasilevich@users.noreply.github.com>
2026-05-27 13:18:25 +0200

    adding a public subnet ID for subnet_id

--- main.tf
+++ main.tf
@@ -33,11 +33,11 @@ module "blog_vpc" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_vpc.public_subnets
+  subnet_id              = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
@@ -54,6 +54,6 @@ module "blog_sg" {
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
